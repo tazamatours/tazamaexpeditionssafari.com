a0e742cf38d1fbad1eba88336975f77b57e65bcf
Tazama Tours <tazama@henryjh.com>
2025-02-11 00:05:55 -0500

    Update deploy configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,8 @@ import mdx from "@astrojs/mdx";
 import icon from "astro-icon";
 import lit from "@astrojs/lit";
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://odyssey-theme.sapling.supply/', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-  sitemap: true, // Generate sitemap (set to "false" to disable)
-  integrations: [sitemap(), mdx(), lit(), icon()], // Add renderers to the config
+  site: `https://tazamaexpeditionssafari.com`, 
+  sitemap: true, 
+  integrations: [sitemap(), mdx(), lit(), icon()]
 });
